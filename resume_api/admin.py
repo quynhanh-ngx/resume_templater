6b45a3f66a271ab46e9a586fac77292eaaa955a5
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-09-24 01:00:49 -0400

    Create model for resume

--- resume_api/admin.py
+++ resume_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from resume_api import models
+
 # Register your models here.
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    pass
--- resume_api/models.py
+++ resume_api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+from dataclasses import dataclass
+from typing import Optional, List
+from datetime import datetime
+
+
+# @dataclass
+# class Award:
+#     name: str
+#     description: str
+#     project_date: Optional[datetime] = None
+#     link: Optional[str] = None
+#
+#
+# @dataclass
+# class Contact:
+#     email: str
+#     phone: str
+#     links: List[str]
+#
+#
+# @dataclass
+# class Education:
+#     name: str
+#     grad_date: datetime
+#     gpa: float
+#     degree: str
+#     major: str
+#     coursework: List[str]
+#
+#
+# @dataclass
+# class SkillCategory:
+#     name: str
+#     skills: List[str]
+#
+#
+# @dataclass
+# class Work:
+#     name: str
+#     location: str
+#     start_date: datetime
+#     description: str
+#     end_date: Optional[datetime] = None
+
+
+class Resume(models.Model):
+    name = models.CharField(max_length=200, help_text="The name of the resume owner")
+    # contact: Contact
+    # education: List[Education]
+    # projects: List[Award]
+    # work: List[Work]
+    # awards: List[Award]
+    # skill_categories: List[SkillCategory]
