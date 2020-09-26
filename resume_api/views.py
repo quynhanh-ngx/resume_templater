516d676bbc517dddd231568d650398f49eeaf518
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-09-26 00:23:38 -0400

    Add simple serializer, route, and view for resume.

--- resume_api/views.py
+++ resume_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Resume
+from .serializers import ResumeSerializer
+from rest_framework import generics
+
 # Create your views here.
+
+class ResumeListCreate(generics.ListCreateAPIView):
+    queryset = Resume.objects.all()
+    serializer_class = ResumeSerializer
+
